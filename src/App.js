ff1b5b7db990542b18fe831ab3a33dd620f54363
alfredkorra <alfredkorra7@gmail.com>
2022-04-30 20:57:18 +0200

    login page UI completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import Login from './Components/LoginPage/Form';
-import { BrowserRouter as Router, Route,  } from "react-router-dom";
-
+import Login from "./Components/LoginPage/Form";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import  Form  from "./Components/LoginPage/Form";
 import Layout from "./Layout";
+import Footer from "./Layout/Footer/Footer";
+import './bootstrap.min.css';
+import './animate.min.css';
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <Layout>
-                    <Router>
-                        <Route path="/login" component={Login}/>
-
-                    </Router>
-
-            </Layout>
-        </div>
-
-    );
+  return (
+    <div className="app">
+{ <Layout>
+        <Router>
+          <Route path="/login" component={Login} />
+          <Route path="/form" component={Form} />
+          <Form></Form>
+        </Router>
+      </Layout> }
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/LoginPage/Form.jsx
+++ src/Components/LoginPage/Form.jsx
@@ -1,182 +1,85 @@
-import React, {Component} from "react";
-import {FormValidation} from "./FormValidation";
-import {connect} from "react-redux";
-import {Container} from "react-bootstrap";
+import React, { Component } from "react";
+import { FormValidation } from "./FormValidation";
 import Logo from "../../Assets/logo.png";
-import api from './../../api';
-import {LOGIN} from "../../redux/auth/actionTypes";
+import api from "./../../api";
+import { LOGIN } from "../../redux/auth/actionTypes";
 import "./Form.css";
+import { useState } from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
+import { toast } from 'react-toastify';
+import { Link } from "react-router-dom";
 
+function Form(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [recaptchaToken, setRecaptchaToken] = useState();
+  const recaptchaRef = React.useRef();
+  const validEmail = String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+  
+  async function handleLogin(e){
 
-// import ReCAPTCHA from "react-google-recaptcha";
+  };
 
-class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            FormValidation: {email: "", password: ""},
-            validateEmail: false,
-            validatePassword: false,
-            validateForm: false,
-        };
-    }
+  const verifyCallback = (recaptchaToken) => {
+    setRecaptchaToken(recaptchaToken);
+  };
 
-
-    validateInputField(inputField, value) {
-        let inputFieldErrors = this.state.FormValidation;
-        let validateEmail = this.state.validateEmail;
-        let validatePassword = this.state.validatePassword;
-
-        switch (inputField) {
-            case "email":
-                validateEmail = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                inputFieldErrors.email = validateEmail ? "" : " is invalid!";
-                break;
-            case "password":
-                validatePassword = value.length >= 6;
-                inputFieldErrors.password = validatePassword ? "" : " is too short!";
-                break;
-            default:
-                break;
-        }
-        this.setState(
-            {
-                FormValidation: inputFieldErrors,
-                validateEmail: validateEmail,
-                validatePassword: validatePassword,
-            },
-        );
-    }
-
-
-    handleInputChange = (event) => {
-        const {name, value} = event.target;
-        this.setState((prevState) => ({
-            ...prevState,
-            [name]: value
-        }))
-        this.validateInputField(name, value);
-    }
-
-    handleLogin = (e) => {
-        e.preventDefault();
-        const {name, password, validateEmail, validatePassword} = this.state;
-        if (!validateEmail || !validatePassword) {
-            return false;
-        }
-
-        const {dispatch} = this.props;
-        const data = {
-            name,
-            password
-        }
-        api.post('/login', data)
-            .then((res) => {
-                dispatch({type: LOGIN.concat("_SUCCESS"), payload: res})
-            })
-            .catch((err) => {
-                console.log('err', err)
-                dispatch({type: LOGIN.concat("_FAILURE")})
-            })
-    }
-
-    render() {
-        const {email, password} = this.state;
-        const {login, logout} = this.props;
-        console.log('props', this.props);
-        return (
-            <Container>
-                <div className="row">
-                    <div className="col-md-12 mx-auto">
-                        <img
-                            alt='logo'
-                            src={Logo}
-                            style={{
-                                backgroundColor: "transparent",
-                                width: "300px",
-                                height: "100px",
-                            }}
-                        />
-                        <div className="myform form ">
-                            <form className="signupForm">
-                                <div className="panel panel-default">
-                                    <FormValidation FormValidation={this.state.FormValidation}/>
-                                </div>
-                                <div className="form-group">
-                                    <br/>
-
-                                    <input
-                                        type="email"
-                                        required
-                                        className="form-control"
-                                        name="email"
-                                        placeholder="Email"
-                                        value={email}
-                                        onChange={(event) => this.handleInputChange(event)}
-                                        style={{
-                                            borderRadius: "12px",
-                                            border: "none",
-                                            fontSize: "16px",
-                                            height: "41px",
-                                            color: '#fff',
-                                            backgroundColor: "#2A2D3C"
-                                        }}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <br/>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        name="password"
-                                        placeholder="Password"
-                                        value={password}
-                                        onChange={(event) => this.handleInputChange(event)}
-                                        style={{
-                                            borderRadius: "12px",
-                                            border: "none",
-                                            fontSize: "16px",
-                                            height: "41px",
-                                            color: '#fff',
-                                            backgroundColor: "#2A2D3C"
-                                        }}
-                                    />
-                                </div>
-                                <br/>
-
-                                <div className="text-center">
-                                    <button
-                                        onClick={(e) => this.handleLogin(e)}
-                                        style={{
-                                            backgroundColor: "#F3C418",
-                                            width: " 100%",
-                                            fontWeight: 600,
-                                            padding: " 10px 25px",
-                                            borderRadius: "10px",
-                                            color: "#000",
-                                        }}
-                                        className="btn"
-                                    >
-                                        Sign In
-                                    </button>
-
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        );
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        auth: state.auth
-    };
-};
-
-
-export default connect(mapStateToProps, null)(Form);
+  return(
+    <main className="main-content">
+    <div className="container">
+    <div className="login-item">
+    <div className="logo-item py-4">
+    <div className="d-flex justify-content-center text-center">
+    <img src="/logo.png" alt="logo" className="logo-main"/>
+    </div>
+    </div>
+    <div className="login-card-form">
+    <h4 className="main-title py-4">
+    Login
+    </h4>
+    <form onSubmit={handleLogin}>
+    <div className="form-group">
+    <input className="form-control" placeholder="Email"
+    type="text"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}/>
+    </div>
+    <div className="form-group">
+    <input className="form-control"  
+    placeholder="Password"
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}/>
+    </div>
+    <div className="form-group">
+    <ReCAPTCHA 
+    ref={recaptchaRef}
+    size="normal"
+    theme="dark"
+    sitekey="6Lem5_cdAAAAAOGTlGUTApVlelyg9wi_XT3H92db"
+    onChange={verifyCallback}>
+    </ReCAPTCHA>
+    </div>
+    <div className="form-group">
+    <button className="btn btn-main btn-lg w-100" type="submit">Sign In</button>
+    </div>
+    </form>
+    <div className="text-left my-4">
+    <div className="mb-1">
+    <Link href={{ pathname: '/forgotPassword' }}> 
+    <a className="register">Forgot password?</a>
+    </Link>
+    </div>
+    <div className="dont">
+    Don't have an account? 
+    <Link href={{ pathname: '/register' }}> 
+    <a className="register"> Register Here</a>
+    </Link>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </main>)
+  }
+  export default Form;
