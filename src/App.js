27b209e4a85ff299d3d00aca3bf16f592ebf7e48
alfredkorra <alfredkorra7@gmail.com>
2022-04-30 21:07:33 +0200

    added loader

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import  Form  from "./Components/LoginPage/Form";
 import Layout from "./Layout";
 import Footer from "./Layout/Footer/Footer";
+import Loader from "./Components/Loader";
 import './bootstrap.min.css';
 import './animate.min.css';
 import "./App.css";
+import jQuery from 'jquery';
+import React, { useEffect } from 'react';
+
 
 function App() {
+  useEffect(()=>{
+    // Animate loader off screen
+    jQuery("#loader-page").delay(100).fadeOut("slow");
+  }, []);
+
   return (
     <div className="app">
 { <Layout>
@@ -17,6 +26,7 @@ function App() {
           <Form></Form>
         </Router>
       </Layout> }
+      <Loader></Loader>
       <Footer />
     </div>
   );
